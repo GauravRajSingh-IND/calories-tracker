c9d9423f1b25e59e0a9f1a99638fd1179c97be68
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-16 12:43:35 +0530

    working on create new user for the application

---
+++ get_calories.py
@@ -0,0 +1,67 @@
+import os
+import requests
+
+END_POINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+
+class Calories:
+
+    def __init__(self):
+
+        self.username = None
+        self.password = None
+        self.email = None
+        self.phone_number = None
+        self.weight_kg = None
+        self.height_cm = None
+        self.age = None
+
+    def register_user(self, user:str, password:str, email:str, weight_kg:float, height_cm:float, age:int,
+                      phone_number:int ):
+        """
+        This function register/ create a user account on the application as well as pixela.
+        :param phone_number: 10 digits number of the user.
+        :param user:  username which will be used while login
+        :param password: password of the user
+        :param email: a valid email id.
+        :param weight_kg: weight in kilograms
+        :param height_cm: height in centimeters
+        :param age: age of the user, should be in int format.
+        :return: dict of the response
+        """
+
+        if not all([user, password, email, phone_number, weight_kg, height_cm, age, phone_number]):
+            raise "Please enter all the required parameters and try again"
+
+        # assign parameters
+        self.username = user
+        self.password = password
+        self.email = email
+        self.weight_kg = weight_kg
+        self.height_cm = height_cm
+        self.age = age
+        self.phone_number = phone_number
+
+        # check if pixela username is already taken.
+        response_pixela_user = requests.get(url=f"https://pixe.la/@{self.username}")
+        if response_pixela_user == 404:
+            is_valid_pixela = True
+
+        else:
+            is_valid_pixela = False
+            raise "username already taken, Please try again with different username."
+
+        # create a user  account in pixela
+
+
+
+client = Calories()
+client.register_user("jobner", "test", "test", 12.5, 12, 12, 1213123)
+
+
+
+
+
+
+
+
