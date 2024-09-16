85de2ae02a624d5deaf6eecd88ae214535990672
Gaurav Raj Singh <grsmanohar@gmail.com>
2024-09-16 18:10:29 +0530

    create landing page buttons and welcome message

--- tkinter_ui.py
+++ tkinter_ui.py
@@ -12,10 +12,29 @@ class UserInterface:
         # background canvas
         self.bg_canvas = tkinter.Canvas(self.window, height=1000, width=1000, highlightthickness=0, bd=0)
         self.bg_canvas.create_image(500, 500, image= self.bg_image)
-        self.bg_canvas.place(x=0, y=0)
+
+        # Add landing page
+        self.landing_page()
 
     def exit_window(self):
         self.window.mainloop()
+
+    def landing_page(self):
+        # welcome text.
+        self.bg_canvas.create_text(500, 150, text="Welcome to Calories Tracker", font=('arial', 25, 'bold'),
+                                   fill="snow")
+        self.bg_canvas.place(x=0, y=0)
+
+        # Button - New User.
+        self.new_user = tkinter.Button(self.window, text="Create a new user", width=20, font=('arial', 25, 'bold'),
+                                       bd=0, highlightthickness=0, height=1)
+        self.new_user.place(x=350, y=455)
+
+        # Button - Existing User
+        self.exist_user = tkinter.Button(self.window, text="Link to existing user", width=20,
+                                         font=('arial', 25, 'bold'),
+                                         bd=0, highlightthickness=0, height=1)
+        self.exist_user.place(x=350, y=540)
 
 
 ui = UserInterface()
